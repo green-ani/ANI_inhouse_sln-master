3c9901a754a0daf539d7abb96d1b1107057f40ca
green-ani <70509266+green-ani@users.noreply.github.com>
2021-07-21 11:46:56 +0800

    add close timer

--- ANI_iot_console/Program.cs
+++ ANI_iot_console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using ani_inhse.Lib;
 
 namespace ANI_iot_console
@@ -12,6 +13,9 @@ namespace ANI_iot_console
         static void Main(string[] args)
         {
             CIot ciot = new CIot();
+            Timer timer = new Timer();
+            timer.Interval = 30000;
+            timer.Elapsed += OnTimedEvent;
 
             try
             {
@@ -26,9 +30,20 @@ namespace ANI_iot_console
             }
             finally
             {
+                Console.WriteLine("Console will close in 30s.");
+                timer.Start();
+                Console.ReadLine();
                 Console.WriteLine("Press enter to close...");
+                timer.Stop();
                 Console.ReadLine();
             }
+        }
+
+        private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Environment.Exit(1);// exit
+
+            throw new NotImplementedException();
         }
     }
 }
